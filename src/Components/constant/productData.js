be83eb8bc0651f33c6a4f75e88beee0c9668fda0
irorere juliet <irorerejuliet@gmail.com>
2025-08-25 17:10:16 +0100

    first commit

---
+++ src/Components/constant/productData.js
@@ -0,0 +1,137 @@
+export const productData = [
+  {
+    id: 1,
+    image: "/images/Product-1.avif",
+    title: "Galaxy Watch6 Classic (Bluetooth, 47mm)",
+    price: 30,
+    categories: "men",
+    description:
+      "Galaxy Watch6 Classic (47mm, Bluetooth): Stylish design, big display, rotating bezel, and smart health tracking.",
+  },
+  {
+    id: 2,
+    image: "/images/Product-2.avif",
+    title: "Galaxy Watch6 Classic (Bluetooth, 47mm)",
+    price: 10,
+    categories: "men",
+    description:
+      "Galaxy Watch6 Classic (47mm, Bluetooth): Stylish design, big display, rotating bezel, and smart health tracking.",
+  },
+  {
+    id: 3,
+    image: "/images/Product-3.webp",
+    title: "Galaxy Watch6 Classic (Bluetooth, 47mm)",
+    price: 40,
+    categories: "men",
+    description:
+      "Galaxy Watch6 Classic (47mm, Bluetooth): Stylish design, big display, rotating bezel, and smart health tracking.",
+  },
+  {
+    id: 4,
+    image: "/images/Product-4.avif",
+    title: "Galaxy Watch6 Classic (Bluetooth, 47mm)",
+    price: 67,
+    categories: "men",
+    description:
+      "Galaxy Watch6 Classic (47mm, Bluetooth): Stylish design, big display, rotating bezel, and smart health tracking.",
+  },
+  {
+    id: 5,
+    image: "/images/Product-5.webp",
+    title: "Galaxy Watch6 Classic (Bluetooth, 47mm)",
+    price: 69,
+    categories: "men",
+    description:
+      "Galaxy Watch6 Classic (47mm, Bluetooth): Stylish design, big display, rotating bezel, and smart health tracking.",
+  },
+  {
+    id: 6,
+    image: "/images/Product-6.png",
+    title: "Galaxy Watch6 Classic (Bluetooth, 47mm)",
+    price: 70,
+    categories: "men",
+    description:
+      "Galaxy Watch6 Classic (47mm, Bluetooth): Stylish design, big display, rotating bezel, and smart health tracking.",
+  },
+  {
+    id: 7,
+    image: "/images/Product-7.png",
+    title: "Galaxy Watch6 Classic (Bluetooth, 47mm)",
+    price: 10,
+    categories: "men",
+    description:
+      "Galaxy Watch6 Classic (47mm, Bluetooth): Stylish design, big display, rotating bezel, and smart health tracking.",
+  },
+  {
+    id: 8,
+    image: "/images/Product-8.png",
+    title: "Galaxy Watch6 Classic (Bluetooth, 47mm)",
+    price: 40,
+    categories: "men",
+    description:
+      "Galaxy Watch6 Classic (47mm, Bluetooth): Stylish design, big display, rotating bezel, and smart health tracking.",
+  },
+  {
+    id: 9,
+    image: "/images/Product-9.webp",
+    title: "Galaxy Watch6 Classic (Bluetooth, 47mm)",
+    price: 56,
+    categories: "men",
+    description:
+      "Galaxy Watch6 Classic (47mm, Bluetooth): Stylish design, big display, rotating bezel, and smart health tracking.",
+  },
+  {
+    id: 10,
+    image: "/images/Product-10.avif",
+    title: "Galaxy Watch6 Classic (Bluetooth, 47mm)",
+    price: 19,
+    categories: "men",
+    description:
+      "Galaxy Watch6 Classic (47mm, Bluetooth): Stylish design, big display, rotating bezel, and smart health tracking.",
+  },
+  {
+    id: 11,
+    image: "/images/Product-11.jpeg",
+    title: "Galaxy Watch6 Classic (Bluetooth, 47mm)",
+    price: 189,
+    categories: "men",
+    description:
+      "Galaxy Watch6 Classic (47mm, Bluetooth): Stylish design, big display, rotating bezel, and smart health tracking.",
+  },
+  {
+    id: 12,
+    image: "/images/Produc-12.jpeg",
+    title: "Galaxy Watch6 Classic (Bluetooth, 47mm)",
+    price: 980,
+    categories: "men",
+    description:
+      "Galaxy Watch6 Classic (47mm, Bluetooth): Stylish design, big display, rotating bezel, and smart health tracking.",
+  },
+  {
+    id: 13,
+    image: "/images/Product-13.jpeg",
+    title: "Galaxy Watch6 Classic (Bluetooth, 47mm)",
+    price: 190,
+    categories: "men",
+    description:
+      "Galaxy Watch6 Classic (47mm, Bluetooth): Stylish design, big display, rotating bezel, and smart health tracking.",
+  },
+  {
+    id: 14,
+    image: "/images/Product-14.jpeg",
+    title: "Galaxy Watch6 Classic (Bluetooth, 47mm)",
+    price: 260,
+    categories: "men",
+    description:
+      "Galaxy Watch6 Classic (47mm, Bluetooth): Stylish design, big display, rotating bezel, and smart health tracking.",
+  },
+  {
+    id: 15,
+    image: "/images/product-15.jpg",
+    title: "Galaxy Watch6 Classic (Bluetooth, 47mm)",
+    price: 320,
+    categories: "men",
+    description:
+      "Galaxy Watch6 Classic (47mm, Bluetooth): Stylish design, big display, rotating bezel, and smart health tracking.",
+  },
+];
