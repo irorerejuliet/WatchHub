c952538c1ce1b0a323c57b6bddc1390ce2475934
irorere juliet <irorerejuliet@gmail.com>
2025-08-25 17:48:46 +0100

    Setting up routes amd components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import Navbar from "./Components/Layouts/Navbar"
+import Footer from "./Components/Layouts/Footer"
+import { Route, Routes } from "react-router-dom"
+import Homepage from "./Pages/Homepage"
+import ProductDetails from "./Pages/ProductDetails"
+import Cart from "./Pages/Cart"
+
 
 const App = () => {
   return (
-    <div>
-
-    </div>
+    <>
+     <Navbar/>
+     <Routes>
+      <Route path="/" element={<Homepage/>}/>
+      <Route path="/product/:id" element={<ProductDetails/>}/>
+      <Route path="/cart" element={<Cart/>}/>
+     </Routes>
+     <Footer/>
+    </>
   )
 }
 
---
+++ src/Components/Layouts/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-red-700'>
+      <h6>juliet</h6>
+    </div>
+  )
+}
+
+export default Footer
