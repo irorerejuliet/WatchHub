f342dca3bba1b1cbd0859da6afa6ce70da5be8b5
irorere juliet <irorerejuliet@gmail.com>
2025-08-31 22:01:21 +0100

    Completed product list

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -11,28 +11,27 @@ const ProductList = () => {
   return (
     <div>
       <div className="max-w-[1740px] m-auto p-20  text-center mt-40">
-        <h2 className="pt-[12em] font-bold text-3xl font-poppins">
+        <h2 className="pt-[12em] font-bold lg:text-3xl text-xl font-poppins">
           Our Elegant & Expectional Collection
         </h2>
-        <div className="grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-10 mt-20">
+        <div className="grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-10 mt-20 ">
           {products.map(({ id, image, title, price }) => (
             <div
               key={id}
-              className="bg-white border border-gray-200 rounded-[8px]"
+              className="bg-white border border-gray-200 rounded-[8px] "
             >
               <Link to={`/product/${id}`}>
                 <img
                   src={image}
                   alt={title}
-                  className="max-w-full max-h-full object-contain text-center m-auto py-4"
-                />
+                  className="max-w-full max-h-full object-contain text-center m-auto py-4 transition-transform duration-150 ease-in-out transform hover:scale-105 " />
                 <div>
                   <h4 className="text-xm font-semibold ">{title}</h4>
-                  <p className="text-[1rem] text-gray-400 py-2">${price}</p>
+                  <p className="text-base text-gray-500 py-2">${price}</p>
                 </div>
               </Link>
               {/* How to write bg transition here */}
-              <button className=" bg-white text-black border border-red-400 cursor-pointer text-[1rem]  hover:bg-red-800 hover:text-white py-2 px-20 ">
+              <button className=" bg-white text-black border border-red-400  py-2 px-20 rounded-[5px] hover:bg-red-600 hover:text-white">
                 Add To Cart
               </button>
             </div>
