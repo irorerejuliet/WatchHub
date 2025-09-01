5e0602250c943b2a61c1d0a7f1571d0026b712fe
irorere juliet <irorerejuliet@gmail.com>
2025-09-01 23:33:41 +0100

    cart

--- src/Components/context/ShopContext.jsx
+++ src/Components/context/ShopContext.jsx
@@ -1,11 +1,33 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { productData } from "../constant/productData";
 
 
 export const ShopContext = createContext();
 
 const ShopContextProvider = ({ children }) => {
+
   const [products, setProducts] = useState(productData);
+  const [cart, setCart] = useState([])
+  const [quantity, setQuantity] = (0)
+  const [total, setTotal] = useState(0)
+
+  // Calculate total price
+  useEffect(() => {
+    
+  })
+
+  // calculate quantity of items in cart 
+
+  // fiuntion to add to cart
+
+  // function to remove from cart 
+
+  //function to clear the entire cart 
+
+  //function to incraese item quantity in cart
+
+  //function to decraese item quantity cart
+
 
   return (
     <ShopContext.Provider value={{ products, setProducts }}>
